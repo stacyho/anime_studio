20535ae74bed598cea7b6444f19cce1e853d1ac5
Stacy Ho <stacyho@mit.edu>
2018-05-22 10:55:11 -0400

    done

--- js/data.js
+++ js/data.js
@@ -2,16 +2,16 @@
 var videoData = [
 	[t(0,9,32), t(0,9,35), "Kyoto Station Platform", "kyoto_station_platform"],
 	[t(0,9,35), t(0,9,45), "Kyoto Station Entrance", "kyoto_station_entrance"],
-	[t(1,11,58), t(1,12,30), "Rokkakudo Temple", "rokkakudo_temple"],
-	[t(1,14,20), t(1,14,23), "Karasuma Street Sign", "karasuma_sign"],
-	[t(1,14,33), t(1,14,48), "Bukkoji Temple", "bukkoji_temple"],
 	[t(0,18,34), t(0,18,37), "Benkei and Yoshitsune", "benkei_and_yoshitsune"],
 	[t(0,19,35), t(0,19,50), "Gojo Bridge", "gojo_bridge"],
 	[t(0,20,34), t(0,20,42), "Gojoten Shrine Entrance", "gojoten_shrine_entrance"],
 	[t(0,20,44), t(0,20,55), "Gojoten Shrine Lions", "gojoten_shrine_lions"],
 	[t(0,21,24), t(0,21,40), "Benkei Stone", "benkei_stone"],
 	[t(0,32,7), t(0,32,12), "Kamo River", "kamo_river"],
-	[t(0,32,12), t(0,32,19), "Pontocho Alley", "pontocho"]
+	[t(0,32,12), t(0,32,19), "Pontocho Alley", "pontocho"],
+	[t(1,11,58), t(1,12,30), "Rokkakudo Temple", "rokkakudo_temple"],
+	[t(1,14,20), t(1,14,23), "Karasuma Street Sign", "karasuma_sign"],
+	[t(1,14,33), t(1,14,48), "Bukkoji Temple", "bukkoji_temple"]
 ];
 
 // startTime, endTime, modelName, fileName
